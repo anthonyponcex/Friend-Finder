d703b4d569678afb4d9c8cc5268df821073cfff7
Anthony Ponce <Anthony Ponce>
2017-06-19 18:18:39 -0700

    updates

--- app/routing/apiRoute.js
+++ app/routing/apiRoute.js
@@ -4,6 +4,7 @@ var friends = require("../data/friends.js")
 //===========================================================
 //Route to display JSON of all possible friends
 module.exports = function (app) {
+	
 app.get("/api/friends", function(req, res) {
 	res.json(friends);
 	console.log(friends);	
@@ -11,15 +12,27 @@ app.get("/api/friends", function(req, res) {
 
 
 // Creates new friend -- takes in JSON input
-app.post("/api/friends", function (req, res) {
+app.post("/api/friends", function(req, res) {
 	//variable to hold new friend into JSON
 	var newFriend = req.body;
 
 	//pushing newFriend into friend JSON
-	friends.push(newFriend)
+	friends.push(newFriend);
 
 	//sending a response to newFriend var as JSON
 	res.json(newFriend);
+
+	//compatability logic
+	var userScores = newFriend.scores;
+
+	for (var i=0; i < req.body.length; i++) {
+		var friendsScores = friends[i].scores;		
+	};
+
+	
+		console.log(userScores);
+	console.log(friendsScores);
+
 });
 
 
